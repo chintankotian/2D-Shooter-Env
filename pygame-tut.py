a598a3e07954556eff2685e83addfd61e7a473e4
chintan <chintankotian>
2020-04-30 19:43:39 +0530

    added bullets and score

--- pygame-tut.py
+++ pygame-tut.py
@@ -2,14 +2,11 @@ import pygame
 display_width = 500
 display_height = 400
 
-win = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('First game')
-pygame.init()
 
-clock = pygame.time.Clock()
+players = []
 
 class Player(object):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, name,score_x,score_y, bullet_interval = 5):
         self.x = x
         self.y = y
         self.width = width
@@ -22,9 +19,30 @@ class Player(object):
         self.jumpCount = 10
         self.bullets = []
         self.hitBox = (self.x + 20, self.y, 20, 40)
+        global players
+        players.append(self)
+        self.name = name
+        self.bullet_interval = bullet_interval
+        self.bullet_shooting_speed = 1
+        self.font = pygame.font.SysFont('comicsans', 20, True)
+        self.score = 0
+        self.score_x = score_x
+        self.score_y = score_y
+        self.name_font = pygame.font.SysFont('comicsans', 10, True)
+
         # self.bullet_velocity = 10
 
     def draw(self,win):
+        text = self.font.render(self.name +" = "+ str(self.score), 1, (0, 0, 0))
+        win.blit(text, (self.score_x, self.score_y))
+        text = self.font.render(self.name, 1, (0, 0, 0))
+        win.blit(text, (self.x, self.y))
+        if(self.bullet_shooting_speed > 0):
+            self.bullet_shooting_speed += 1
+        
+        if(self.bullet_shooting_speed > self.bullet_interval):
+            self.bullet_shooting_speed = 0
+        
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
         if self.right:
@@ -36,7 +54,7 @@ class Player(object):
         else:
             win.blit(char,(self.x,self.y))
         self.hitBox = (self.x + 20, self.y + 15, 20, 45)
-        pygame.draw.rect(win, (255,0,0), self.hitBox, 2)
+        pygame.draw.rect(win, (255,255,0), self.hitBox, 2)
 
     def step_left(self):
         if(self.x >= self.velocity):
@@ -53,16 +71,16 @@ class Player(object):
     def halt(self):
         # self.left = False
         # self.right = False
-        if(self.left):
-            win.blit(walkLeft[0],(self.x,self.y))
-        else:
+        if(self.right):
             win.blit(walkRight[0],(self.x,self.y))
+        elif(self.left):
+            win.blit(walkLeft[0],(self.x,self.y))
 
         self.walkCount = 0
     
     def jump(self):
         self.isJump = True
-        self.halt()
+        # self.halt()
     
     def cont_jump(self):
         neg = 1
@@ -77,11 +95,12 @@ class Player(object):
                 self.isJump = False
                 self.jumpCount = 10
     def shoot(self):
-        if(len(self.bullets) < 5):
-            if(self.left):
-                direction = 1
-            else:
+        if(len(self.bullets) < 1):
+            self.bullet_shooting_speed = 1
+            if(self.right):
                 direction = -1
+            else:
+                direction = 1
 
             self.bullets.append(bullet(self.x + self.width/2, self.y + self.height/2, direction, self))
 
@@ -92,14 +111,18 @@ class bullet():
         self.direction = direction
         self.velocity = 10
         self.player = player
+        self.radius = 5
         
+    def bullet_pop(self):
+        self.player.bullets.pop(self.player.bullets.index(self))
+
     def draw(self,win):
         global display_width
         if(self.x > 0) and (self.x < display_width):
             self.x -= self.velocity * self.direction
-            pygame.draw.circle(win, (0,0,0), (int(self.x),int(self.y)), 5)
+            pygame.draw.circle(win, (0,0,0), (int(self.x),int(self.y)), self.radius)
         else:
-            self.player.bullets.pop(self.player.bullets.index(self))
+            self.bullet_pop()
         # else:
             # self.bullets
 #Parametrs of the object
@@ -111,8 +134,15 @@ char = pygame.image.load('standing.png')
 
 run = True
 
-man1 = Player(0,display_height - 64,64,64)
-man2 = Player(display_width - 64,display_height - 64,64,64)
+def collision(bullet):
+    # iterates through all the bullets and checks whether it has collided with the opponents hit box
+    for player in players:
+        if(player != bullet.player):
+            if(bullet.y - bullet.radius < player.hitBox[1] + player.hitBox[3]) and (bullet.y + bullet.radius > player.hitBox[1]):
+                if(bullet.x + bullet.radius > player.hitBox[0]) and (bullet.x - bullet.radius < player.hitBox[0] + player.hitBox[2]):
+                    print(bullet.player.name + " hit "+ player.name)
+                    bullet.bullet_pop()
+                    bullet.player.score += 1
 # refresh state
 def redrawWindow(mans,win):
     global walkCount
@@ -123,63 +153,85 @@ def redrawWindow(mans,win):
         total_bullets += man.bullets
 
     for shot_bullet in total_bullets:
+        collision(shot_bullet)
+    
+    for shot_bullet in total_bullets:
         shot_bullet.draw(win)
 
     pygame.display.update()
-
-# main loop
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        # print(event)
-        if(event.type == pygame.QUIT):
-            run = False
-
-    keys = pygame.key.get_pressed()
-    # man 1 control
-    if(keys[pygame.K_LEFT]):
-        man1.step_left()
-
-    elif(keys[pygame.K_RIGHT]):
-        man1.step_right(display_width)
-
-    else:
-        man1.halt()
-        # pass
-
-    # if not man1.isJump:
-    if(keys[pygame.K_SPACE]):
-        man1.shoot()
-
-    if(keys[pygame.K_UP]):
-        man1.jump()
     
+   
+
+if __name__ == '__main__':
+    win = pygame.display.set_mode((display_width,display_height))
+    pygame.display.set_caption('First game')
+    pygame.init()
+    clock = pygame.time.Clock()
     
+    man1 = Player(0,display_height - 64,64,64, 'player-1', 10, 10)
+    man2 = Player(display_width - 64,display_height - 64,64,64, 'player-2', 390, 10)
+    # main loop
+    while run:
+        clock.tick(60)
+        for event in pygame.event.get():
+            # print(event)
+            if(event.type == pygame.QUIT):
+                run = False
 
-    # man1.cont_jump()
-    # man2.cont_jump()
-    
-    # man 2 control
-    if(keys[pygame.K_a]):
-        man2.step_left()
+        keys = pygame.key.get_pressed()
+        # man 1 control
+        if(keys[pygame.K_RIGHT]):
+            man1.step_right(display_width)
 
-    elif(keys[pygame.K_d]):
-        man2.step_right(display_width)
 
-    else:
-        man2.halt()
+        elif(keys[pygame.K_LEFT]):
+            man1.step_left()
 
-    # if not isJump:
-    if(keys[pygame.K_w]):
-        man2.jump()
 
-    if(keys[pygame.K_TAB]):
-        man2.shoot()
+        else:
+            man1.halt()
+            # pass
 
-    man1.cont_jump()
-    man2.cont_jump()
+        if not man1.isJump:
+            if(keys[pygame.K_UP]):
+                man1.jump()
 
-    redrawWindow([man1,man2],win)
-    # redrawWindow(man2,win)
-    
+        if(keys[pygame.K_RCTRL])  and (man1.bullet_shooting_speed == 0):
+            man1.shoot()
+
+
+
+
+
+
+        
+        
+
+        # man1.cont_jump()
+        # man2.cont_jump()
+        
+        # man 2 control
+        if(keys[pygame.K_a]):
+            man2.step_left()
+
+        elif(keys[pygame.K_d]):
+            man2.step_right(display_width)
+
+        else:
+            man2.halt()
+
+        # if not isJump:
+        if(keys[pygame.K_w]):
+            man2.jump()
+
+        if(keys[pygame.K_LCTRL]) and  (man2.bullet_shooting_speed == 0):
+            man2.shoot()
+
+        man1.cont_jump()
+        man2.cont_jump()
+
+
+        redrawWindow(players,win)
+        # redrawWindow(man2,win)
+        
 
